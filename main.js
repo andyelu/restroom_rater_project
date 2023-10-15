4f0a720941bc368be6c35bf4ae44e01bfcd04162
Andy Lu <andylu@Andys-MacBook-Air.local>
2023-10-14 21:48:15 -0700

    added map

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-import './style.css'
+require("dotenv").config();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    
-  </div>
-`
+const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 20,
+    center: { lat: 32.2319, lng: -110.9527 },
+    mapTypeControl: false,
+    streetViewControl: false,
+    fullscreenControl: false,
+    keyboardShortcuts: false,
+  });
+}
