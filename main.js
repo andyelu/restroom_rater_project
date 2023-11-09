302c8ab88022ea22ab180e2e2c6ee7af1c057521
Nathan Kumar <nathanakumar@arizona.edu>
2023-11-08 21:33:24 -0700

    Updated Find Nearby with Map

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 20,
+    zoom: 10,
     center: { lat: 32.2319, lng: -110.9527 },
     mapTypeControl: false,
     streetViewControl: false,
