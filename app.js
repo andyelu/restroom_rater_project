9ef6280938b0a4809b96143ac40413901b43da6c
Nathan Kumar <nathanakumar@arizona.edu>
2023-12-18 15:58:06 -0700

    Updated Map Btn

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const rateBtn = document.getElementById("rate-btn");
 const mapBtn = document.getElementById("map-btn");
 const reviewCardTemplate = document.querySelector("[data-review-template]");
-const reviewCardContainer = document.querySelector(
-  "[data-review-cards-container]"
-);
+const reviewCardContainer = document.querySelector("[data-review-cards-container]");
 
 rateBtn.onclick = () => {
   window.location.href = `rate.html?id=${getRestroomNameFromUrl()}`;
-};
-
-mapBtn.onclick = () => {
-  window.location.href = `nearby.html?location=${location}`;
 };
 
 let reviews = [];
@@ -20,20 +14,36 @@ function getRestroomNameFromUrl() {
   return decodeURIComponent(urlParams.get("id"));
 }
 
-const getRating = () => {
-  // Directly return the Promise from axios.get
-  return axios.get(
-    `http://3.101.24.44:8080/api/v1/reviews/rating/${getRestroomNameFromUrl()}`
-  );
+mapBtn.onclick = () => {
+  getLocation().then((location) => {
+    window.location.href = `https://www.google.com/maps?q=${location}`;
+  }).catch((error) => {
+    console.error("Error:", error);
+  });
 };
 
-const getReviews = () => {
-  return axios
-    .get(`http://3.101.24.44:8080/api/v1/reviews/${getRestroomNameFromUrl()}`)
+const getRating = () => {
+  // Directly return the Promise from axios.get
+  return axios.get(`http://3.101.24.44:8080/api/v1/reviews/rating/${getRestroomNameFromUrl()}`);
+};
+
+const getLocation = () => {
+  return axios.get(`http://3.101.24.44:8080/api/v1/restroom/search?name=${getRestroomNameFromUrl()}`)
     .then((response) => {
       const jsonData = response.data;
-      return jsonData;
+      const location = jsonData.location;
+      console.log(location);
+      console.log(jsonData);
+      return location;
     });
+};
+
+
+const getReviews = () => {
+  return axios.get(`http://3.101.24.44:8080/api/v1/reviews/${getRestroomNameFromUrl()}`).then((response) => {
+    const jsonData = response.data;
+    return jsonData;
+  });
 };
 
 const displayRestroomName = () => {
@@ -56,9 +66,9 @@ const displayReviews = () => {
         const rating = card.querySelector("[data-rating]");
         const comment = card.querySelector("[data-comment]");
         const tags = card.querySelector("[data-tags]");
-        const accessible = card.querySelector("[data-accessible]")
-        const genderNeutral = card.querySelector("[data-genderNeutral]")
-        const gender = card.querySelector("[data-gender]")
+        const accessible = card.querySelector("[data-accessible]");
+        const genderNeutral = card.querySelector("[data-genderNeutral]");
+        const gender = card.querySelector("[data-gender]");
         const date = card.querySelector("[data-date]");
         rating.textContent = review.rating;
         comment.textContent = review.comment;
@@ -75,9 +85,9 @@ const displayReviews = () => {
         }
 
         const tagElements = [];
-        if (review.tag1 != '' && review.tag1 !== null) tagElements.push(createTagElement(review.tag1));
-        if (review.tag2 != '' && review.tag2 !== null) tagElements.push(createTagElement(review.tag2));
-        if (review.tag3 != '' && review.tag3 !== null) tagElements.push(createTagElement(review.tag3));
+        if (review.tag1 != "" && review.tag1 !== null) tagElements.push(createTagElement(review.tag1));
+        if (review.tag2 != "" && review.tag2 !== null) tagElements.push(createTagElement(review.tag2));
+        if (review.tag3 != "" && review.tag3 !== null) tagElements.push(createTagElement(review.tag3));
         tags.append(...tagElements);
 
         if (review.wheelChair === true) {
